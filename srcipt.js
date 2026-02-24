0d890c6d22e2d5163c0b54ee4db96e428f3b113b
Badara <bbabadara@gmail.com>
2026-02-24 15:22:45 +0000

    first 4

--- srcipt.js
+++ srcipt.js
@@ -1,4 +1,10 @@
 const lienA=document.getElementById("lienA");
+const btn=document.getElementById("btn");
+const div1=document.getElementById("div1");
 lienA.addEventListener("click",function(){
     alert("Cliqué");
 })  
+
+btn.addEventListener("click",function(){
+    div1.innerHTML+="Cliqué";
+})
